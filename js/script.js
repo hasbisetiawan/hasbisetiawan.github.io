73a4ff30f2bc634b0a629e25385ac26bccbf40d9
Hasbi Setiawan <64304827+hasbisetiawan@users.noreply.github.com>
2021-11-04 22:22:39 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,51 +1,24 @@
-$(window).on('load', function() {
-	$('.pKiri').addClass('pMuncul');
-	$('.pKanan').addClass('pMuncul');
-});
+// membuat theme dark
+let themeSwitch = document.querySelector('.themeSwitch');
+let body = document.querySelector('body');
+let navbar = document.querySelector('.navbar');
+themeSwitch.onclick = function(){
+  themeSwitch.classList.toggle('active');
+  body.classList.toggle('dark');
+  navbar.classList.toggle('navbar-dark');
+}
+
 // elmen pada saat link di klik
 $('.page-scroll').on('click', function(e){
-	// ambil isi href
-	var tujuan = $(this).attr('href');
+  // ambil isi href
+  var tujuan = $(this).attr('href');
 
-	var elemenTujuan = $(tujuan);
+  var elemenTujuan = $(tujuan);
 
-	// pindahkan scroll
-	$('html,body').animate({
-		scrollTop: elemenTujuan.offset().top - 50
-	},'swing');
+  // pindahkan scroll
+  $('html,body').animate({
+    scrollTop: elemenTujuan.offset().top - 70
+  },'swing');
 
-	e.preventDefault();
- });
-
-// memberi paralaxx
-
-$(window).scroll(function() {
-	var wScroll = $(this).scrollTop();
-	$('.jumbotron img').css({
-		'transform' : 'translate(0px, '+ wScroll/4 +'%)'
-	});
-
-	$('.jumbotron h1').css({
-		'transform' : 'translate(0px, '+ wScroll/2 +'%)'
-	});
-
-	$('.jumbotron p').css({
-		'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
-	});
-
-// about
-
-// portfolio
-
-if( wScroll > 800 ) {
-	$('.portfolio .thumbnail').each(function(i) {
-		setTimeout(function() {
-			$('.portfolio .thumbnail').eq(i).addClass('muncul');
-		}, 300 * (i+1));
-	});
-
-	
-};
-
-
+  e.preventDefault();
 });
