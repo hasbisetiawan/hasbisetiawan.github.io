0d89a9f1fb4be8bba9a0ea9d43f8dbf353019dbf
Hasbi Setiawan <64304827+hasbisetiawan@users.noreply.github.com>
2021-11-08 09:56:46 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,3 +22,32 @@ $('.page-scroll').on('click', function(e){
 
   e.preventDefault();
 });
+
+// form contact
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwct8XS85l_RXi0YegL47FoMjCufeqvYdX6-omrvz5if5su1u2s7uqh6PNGonEv_-t8/exec';
+const form = document.forms['portfolio-contact'];
+
+const btnKirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
+const myAlert = document.querySelector('.my-alert');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  // ketika tombol submit di klik
+  // tampilkan tombol loading, hilangkan tommbol kirim
+  btnLoading.classList.toggle('d-none');
+  btnKirim.classList.toggle('d-none');
+
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      // tampilkan tombol kirim, hilangkan tombol kirim
+      btnLoading.classList.toggle('d-none');
+      btnKirim.classList.toggle('d-none');
+      // tampilkan alert
+      myAlert.classList.toggle('d-none');
+      // reset form
+      form.reset();
+      console.log('Success!', response)
+    })
+    .catch(error => console.error('Error!', error.message))
+});
