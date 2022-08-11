8340a7e942fbb168dda11fe374cbccb331f11fed
Hasbi Setiawan <64304827+hasbisetiawan@users.noreply.github.com>
2022-08-12 01:37:02 +0700

    Create view.php

---
+++ view.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hasbi Setiawan</title>
+</head>
+<body style="margin: 0; padding: 0; background: #525659;">
+		<object data="cv.pdf" type="application/pdf" width="100%" height="1000">
+		</object>
+</body>
+</html
